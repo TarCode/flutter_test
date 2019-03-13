7a413ffcbe991d6e81cfd855176755e798a0288b
TarCode <tarcode33@gmail.com>
2019-03-13 18:07:43 +0200

    updated model and created list of model instances in stateful home widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // main.dart
 import 'package:flutter/material.dart';
+import 'dog_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,6 +29,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+    List<Dog> initialDoggos = []
+      ..add(Dog('Ruby', 'Portland, OR, USA',
+          'Ruby is a very good girl. Yes: Fetch, loungin\'. No: Dogs who get on furniture.'))
+      ..add(Dog('Rex', 'Seattle, WA, USA', 'Best in Show 1999'))
+      ..add(Dog('Rod Stewart', 'Prague, CZ',
+          'Star good boy on international snooze team.'))
+      ..add(Dog('Herbert', 'Dallas, TX, USA', 'A Very Good Boy'))
+      ..add(Dog('Buddy', 'North Pole, Earth', 'Self proclaimed human lover.'));
+
     @override
     Widget build(BuildContext context) {
         /// Scaffold is the base for a page.
